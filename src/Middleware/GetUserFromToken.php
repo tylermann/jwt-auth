af0f599a24acb7e2ccf32c9366473b64c6a4563b
Tyler Mann <tyler@pullrequest.com>
2019-04-24 10:56:36 -0500

    use events->dispatch instead of events->fire for laravel 5.8 support

--- src/Middleware/GetUserFromToken.php
+++ src/Middleware/GetUserFromToken.php
@@ -41,7 +41,13 @@ class GetUserFromToken extends BaseMiddleware
             return $this->respond('tymon.jwt.user_not_found', 'user_not_found', 404);
         }
 
-        $this->events->fire('tymon.jwt.valid', $user);
+        // the Dispatcher::fire method was removed in Laravel 5.8 https://laravel.com/docs/5.8/upgrade#events
+        if (method_exists($this->events, 'fire')) {
+            $this->events->fire('tymon.jwt.valid', $user);
+        } else {
+            $this->events->dispatch('tymon.jwt.valid', $user);
+        }
+
 
         return $next($request);
     }
